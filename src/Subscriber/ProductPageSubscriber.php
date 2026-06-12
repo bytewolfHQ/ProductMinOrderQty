9dfc72df6f1251624f04a35ca7db7232e5e54908
WolfgangT <wolfgang.tippelt@web.de>
2026-06-12 15:12:41 +0200

    Add product min qty

--- src/Subscriber/ProductPageSubscriber.php
+++ src/Subscriber/ProductPageSubscriber.php
@@ -7,6 +7,8 @@ use Shopware\Storefront\Page\Product\ProductPageLoadedEvent;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 
 class ProductPageSubscriber implements EventSubscriberInterface {
+    private const CUSTOM_FIELD_NAME = 'bytewolfhq_min_order_qty';
+
     public function __construct(
         private readonly EntityRepository $productRepository
     )
@@ -22,6 +24,20 @@ class ProductPageSubscriber implements EventSubscriberInterface {
 
     public function onProductPageLoaded(ProductPageLoadedEvent $event): void
     {
-        // TODO: Set min attribute on quantity input
+        $product = $event->getPage()->getProduct();
+
+        $customFields = $product->getCustomFields() ?? [];
+        $minQty = (int) ($customFields[self::CUSTOM_FIELD_NAME] ?? 0);
+
+        if ($minQty < 0) {
+            return;
+        }
+
+        // Overwrite current minPurchase if minQty is greater
+        $currentMin = $product->getMinPurchase() ?? 1;
+
+        if ($minQty > $currentMin) {
+            $product->setMinPurchase($minQty);
+        }
     }
 }
